4385ecbe251e4cbe670bae7cb71f3a6393210c4e
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-22 18:02:57 -0300

    1.1

--- js/produtos.js
+++ js/produtos.js
@@ -1,10 +1,8 @@
-const produtos = [
-    {
+const produtos = [{
         "id": 1,
         "nome": "Casaco com bolsos laterais",
         "categoria": "Casaco",
-        "notas": [
-            {
+        "notas": [{
                 nota: "5"
             },
             {
@@ -20,15 +18,14 @@ const produtos = [
                 nota: "3"
             }
         ],
-        "imagens": "arquivos/produtos/L1-2.jpg",
+        "imagens": "produtos/L1-2.jpg",
         "precoMedio": 4,
         "disponivel": true
     }, {
         "id": 2,
         "nome": "Blusa com gola arredondada",
         "categoria": "Blusa",
-        "notas": [
-            {
+        "notas": [{
                 nota: 4,
             },
             {
@@ -41,54 +38,48 @@ const produtos = [
                 nota: 5,
             }
         ],
-        "imagens": "arquivos/produtos/L2-2.jpg",
+        "imagens": "produtos/L2-2.jpg",
         "precoMedio": 2,
         "disponivel": true
     }, {
         "id": 3,
         "nome": "Vestido tubinho",
         "categoria": "Vestido",
-        "notas": [
-            {
-                nota: 4,
-            },
-        ],
-        "imagens": "arquivos/produtos/L3-1.jpg",
+        "notas": [{
+            nota: 4,
+        }, ],
+        "imagens": "produtos/L3-1.jpg",
         "precoMedio": 4,
         "disponivel": false
     }, {
         "id": 4,
         "nome": "Calça social com bolsos",
         "categoria": "Calça",
-        "notas": [
-            {
+        "notas": [{
                 nota: 5,
             },
             {
                 nota: 3
             }
         ],
-        "imagens": "arquivos/produtos/L4-1.jpg",
+        "imagens": "produtos/L4-1.jpg",
         "precoMedio": 3,
         "disponivel": true
     }, {
         "id": 5,
         "nome": "Blusa de algodão",
         "categoria": "Blusa",
-        "notas": [
-            {
-                nota: 4,
-            }
-        ],
-        "imagens": "arquivos/produtos/L5-1.jpg",
+        "notas": [{
+            nota: 4,
+        }],
+        "imagens": "produtos/L5-1.jpg",
         "precoMedio": 1,
         "disponivel": false
     }, {
         "id": 6,
         "nome": "Body preto básico",
         "categoria": "Body",
-        "notas": [
-            {
+        "notas": [{
                 nota: 1,
             },
             {
@@ -113,15 +104,14 @@ const produtos = [
                 nota: 5
             }
         ],
-        "imagens": "arquivos/produtos/L6-1.jpg",
+        "imagens": "produtos/L6-1.jpg",
         "precoMedio": 3,
         "disponivel": false
     }, {
         "id": 7,
         "nome": "Body cavado",
         "categoria": "Body",
-        "notas": [
-            {
+        "notas": [{
                 nota: 1,
             },
             {
@@ -146,15 +136,14 @@ const produtos = [
                 nota: 5
             }
         ],
-        "imagens": "arquivos/produtos/L7-1.jpg",
+        "imagens": "produtos/L7-1.jpg",
         "precoMedio": 4,
         "disponivel": true
     }, {
         "id": 8,
         "nome": "Blusa ciganinha",
         "categoria": "Blusa",
-        "notas": [
-            {
+        "notas": [{
                 nota: 4,
             },
             {
@@ -170,22 +159,21 @@ const produtos = [
                 nota: 4
             }
         ],
-        "imagens": "arquivos/produtos/L8-1.jpg",
+        "imagens": "produtos/L8-1.jpg",
         "precoMedio": 2,
         "disponivel": false
     }, {
         "id": 9,
         "nome": "Casaco slim com bolsos",
         "categoria": "Casaco",
-        "notas": [
-            {
+        "notas": [{
                 nota: 5,
             },
             {
                 nota: 5
             }
         ],
-        "imagens": "arquivos/produtos/L9-1.jpg",
+        "imagens": "produtos/L9-1.jpg",
         "precoMedio": 4,
         "disponivel": true
     },
@@ -193,8 +181,7 @@ const produtos = [
         "id": 10,
         "nome": "Vestido com detalhes em renda",
         "categoria": "Vestido",
-        "notas": [
-            {
+        "notas": [{
                 nota: 1,
             },
             {
@@ -219,15 +206,14 @@ const produtos = [
                 nota: 5
             }
         ],
-        "imagens": "arquivos/produtos/L10-1.jpg",
+        "imagens": "produtos/L10-1.jpg",
         "precoMedio": 2,
         "disponivel": false
     }, {
         "id": 11,
         "nome": "Blusa branca com corte",
         "categoria": "Blusa",
-        "notas": [
-            {
+        "notas": [{
                 nota: 5,
             },
             {
@@ -249,19 +235,17 @@ const produtos = [
                 nota: 5
             }
         ],
-        "imagens": "arquivos/produtos/L11-1.jpg",
+        "imagens": "produtos/L11-1.jpg",
         "precoMedio": 1,
         "disponivel": true
     }, {
         "id": 12,
         "nome": "Calça jeans rasgada com detalhes",
         "categoria": "Calça",
-        "notas": [
-        ],
-        "imagens": "arquivos/produtos/L12-1.jpg",
- 
+        "notas": [],
+        "imagens": "produtos/L12-1.jpg",
+
         "precoMedio": 3,
         "disponivel": false
     },
 ]
-
